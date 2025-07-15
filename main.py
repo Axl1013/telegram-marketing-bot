f2070601e7ffba0a27d43526f4cbca0cef5d84aa
Axl1013 <145440781+Axl1013@users.noreply.github.com>
2025-07-15 17:13:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,6 @@ async def main():
     logging.info("✅ Bot gestart...")
     await bot_app.run_polling()
 if __name__ == "__main__":
-    keep_alive()
     nest_asyncio.apply()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
